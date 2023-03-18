c66756c6e43619c8b9e8b02bf2042af0cf5bf838
MrBlackout1 <mrbobfish0@gmail.com>
2023-03-18 19:27:58 +0200

    first commit

---
+++ Laba 4.2 2023/EventBus.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4._2_2023
+{
+    public class EventBus
+    {
+        private Dictionary<string, List<Delegate>> _eventHandlers;
+        private DateTime _lastEventTime;
+        private int _eventThrottleMilliseconds;
+
+        public EventBus(int eventThrottleMilliseconds)
+        {
+            _eventHandlers = new Dictionary<string, List<Delegate>>();
+            _eventThrottleMilliseconds = eventThrottleMilliseconds;
+            _lastEventTime = DateTime.MinValue;
+        }
+
+        public void RegisterEventHandler(string eventName, Delegate eventHandler)
+        {
+            if (!_eventHandlers.ContainsKey(eventName))
+            {
+                _eventHandlers[eventName] = new List<Delegate>();
+            }
+
+            _eventHandlers[eventName].Add(eventHandler);
+        }
+
+        public void UnregisterEventHandler(string eventName, Delegate eventHandler)
+        {
+            if (_eventHandlers.ContainsKey(eventName))
+            {
+                _eventHandlers[eventName].Remove(eventHandler);
+            }
+        }
+
+        public void RaiseEvent(string eventName, object sender, EventArgs args)
+        {
+            if (_eventHandlers.ContainsKey(eventName))
+            {
+                var eventHandlers = _eventHandlers[eventName];
+
+                var timeSinceLastEvent = DateTime.Now - _lastEventTime;
+                if (timeSinceLastEvent.TotalMilliseconds < _eventThrottleMilliseconds)
+                {
+                    var waitTime = _eventThrottleMilliseconds - timeSinceLastEvent.TotalMilliseconds;
+                    Thread.Sleep((int)waitTime);
+                }
+                foreach (var eventHandler in eventHandlers)
+                {
+                    eventHandler.DynamicInvoke(sender, args);
+                }
+
+                _lastEventTime = DateTime.Now;
+            }
+        }
+
+        public class Publisher
+        {
+            private Dictionary<string, List<Action>> _eventHandlers;
+            public Publisher()
+            {
+                _eventHandlers = new Dictionary<string, List<Action>>();
+            }
+            public void RegisterEventHandler(string eventName, Action eventHandler, int priority)
+            {
+                if (!_eventHandlers.ContainsKey(eventName))
+                {
+                    _eventHandlers[eventName] = new List<Action>();
+                }
+                var handlers = _eventHandlers[eventName];
+                var index = handlers.FindIndex(h => h.Target == null || h.Target.Equals(eventHandler.Target) && h.Method.Equals(eventHandler.Method));
+                if (index < 0)
+                {
+                    index = handlers.Count;
+                }
+                handlers.Insert(index, eventHandler);
+            }
+            public void UnregisterEventHandler(string eventName, Action eventHandler)
+            {
+                if (_eventHandlers.ContainsKey(eventName))
+                {
+                    var handlers = _eventHandlers[eventName];
+                    handlers.RemoveAll(h => h.Target == null || h.Target.Equals(eventHandler.Target) && h.Method.Equals(eventHandler.Method));
+                }
+            }
+
+            public void RaiseEvent(string eventName)
+            {
+                if (_eventHandlers.ContainsKey(eventName))
+                {
+                    var handlers = _eventHandlers[eventName];
+                    foreach (var handler in handlers)
+                    {
+                        handler();
+                    }
+                }
+            }
+        }
+        public class Subscriber
+        {
+            private Publisher _publisher;
+            private int _priority;
+
+            public Subscriber(Publisher publisher, int priority)
+            {
+                _publisher = publisher;
+                _priority = priority;
+            }
+
+            public void Subscribe(string eventName, Action eventHandler)
+            {
+                _publisher.RegisterEventHandler(eventName, eventHandler, _priority);
+            }
+
+            public void Unsubscribe(string eventName, Action eventHandler)
+            {
+                _publisher.UnregisterEventHandler(eventName, eventHandler);
+            }
+        }
+    }
+}
---
+++ Laba 4.2 2023/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using static Laba_4._2_2023.EventBus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var publisher = new Publisher();
+
+        var subscriber1 = new Subscriber(publisher, 1);
+        var subscriber2 = new Subscriber(publisher, 2);
+        var subscriber3 = new Subscriber(publisher, 3);
+
+        subscriber1.Subscribe("event1", () => Console.WriteLine("Event1 handler 1 called"));
+        subscriber2.Subscribe("event1", () => Console.WriteLine("Event1 handler 2 called"));
+        subscriber2.Subscribe("event2", () => Console.WriteLine("Event2 handler 1 called"));
+        subscriber3.Subscribe("event1", () => Console.WriteLine("Event1 handler 3 called"));
+        subscriber3.Subscribe("event2", () => Console.WriteLine("Event2 handler 2 called"));
+
+        publisher.RaiseEvent("event1");
+        publisher.RaiseEvent("event2");
+
+        subscriber1.Unsubscribe("event1", () => Console.WriteLine("Event1 handler 1 called"));
+
+        publisher.RaiseEvent("event1");
+        publisher.RaiseEvent("event2");
+    }
+}
